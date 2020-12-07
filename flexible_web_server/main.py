1a736dfb80d6071a4c76b625d1a3b2c7910e013e
al_headstrong <alexdfilson@gmail.com>
2020-12-07 10:28:34 -0800

    Updated flexible web server 'main.py'.

--- flexible_web_server/main.py
+++ flexible_web_server/main.py
@@ -30,6 +30,10 @@ except:
     seconds = 0
 rtc.datetime(utime.localtime(seconds))
 
+pin = machine.Pin(2, machine.Pin.OUT)
+adc = machine.ADC(0)
+switch_pin = machine.Pin(10, machine.Pin.IN)
+
 def time():
     body = """<html>
 <body>
@@ -41,15 +45,43 @@ def time():
 
     return response_template % body
 
+
 def dummy():
     body = "This is a dummy endpoint"
-
     return response_template % body
+
+
+def light_on():
+    pin.value(1)
+    body = "You turned a light on!"
+    return response_template % body
+
+
+def light_off():
+    pin.value(0)
+    body = "You turned a light off!"
+    return response_template % body
+
+
+def switch():
+    body = "{state: " + str(switch_pin.value()) + "}"
+    return response_template % body
+
+
+def light():
+    body = "{value: " + str(adc.read()) + "}"
+    return response_template % body
+
 
 handlers = {
     'time': time,
     'dummy': dummy,
+    'light_on': light_on,
+    'light_off': light_off,
+    'switch': switch,
+    'light': light,
 }
+
 
 def main():
     s = socket.socket()
